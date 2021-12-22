016ab8e7986f6f0f4fd07f676637b6751a07e65e
PythonCoderAS <sarkaraoyan@gmail.com>
2021-12-21 22:30:26 -0500

    Create sum134.py

---
+++ Dynamic Programming 2/sum134.py
@@ -0,0 +1,9 @@
+data = {():0}
+def sticker_max(lst):
+    if lst not in data:
+        data[lst] = max(lst[0] + sticker_max(lst[2:]), sticker_max(lst[1:]))
+    return data[lst]
+
+print(sticker_max((6, 1, 1, 3))) # should be 9
+print(sticker_max((4, 1, 2, 10, 9))) # should be 15
+print(sticker_max((1, 2, 3, 4))) # should be 6
