72590836eaabfbd3642de1bc1e6d8d6aadfb464d
PythonCoderAS <sarkaraoyan@gmail.com>
2021-12-22 10:06:07 -0500

    Create split_link.py

---
+++ Dynamic Programming Optimization 1/split_link.py
@@ -0,0 +1,10 @@
+def split_link(prices):
+    """Splits the chain on a link. Variable prices is an array of possible prices."""
+    data = [0] * (len(prices) + 4)
+    current_index = len(prices) - 3
+    while current_index >= 0:
+        option_1 = sum(prices[current_index:current_index+3]) + data[current_index + 4]
+        option_2 = sum(prices[current_index:current_index+2]) + data[current_index + 3]
+        data[current_index] = max(option_1, option_2)
+        current_index -= 1
+    return data[0]
